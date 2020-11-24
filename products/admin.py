3c1fb5b2db668a9a7b75154847775775c5f835ba
MauRuRo <m.rubenderoos@gmail.com>
2020-11-24 20:35:54 +0000

    added produt views and templates

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'sku', 
+        'sku',
         'name',
         'category',
         'price',
@@ -13,11 +14,13 @@ class ProductAdmin(admin.ModelAdmin):
 
     oredering = ('sku',)
 
+
 class CategoryAdmin(admin.ModelAdmin):
-       list_display = (
+    list_display = (
         'friendly_name',
         'name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
