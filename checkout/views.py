228356dac2045855c582f9bec3ffe9976d0d503e
MauRuRo <m.rubenderoos@gmail.com>
2020-11-27 21:26:47 +0000

    setup stripe

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HsDgjDkrpalIwBc7BEDcca6MTlERYinria7OXBUoKsGDYEqsFGJIzQrsAZgIbwy0r7H8xckVrwnN55KUZi7UKVx00YYVxm0aD',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
