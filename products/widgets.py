1d6cab736df1fb2bc8bdb08fd248c8b8e373b8d3
MauRuRo <m.rubenderoos@gmail.com>
2020-12-05 18:36:41 +0000

    finished product form image field

--- products/widgets.py
+++ products/widgets.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
-    template_name = 'products/custom_widget_template/custom_clearable_file_input.html'
-
+    input_text = _('')
+    template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
