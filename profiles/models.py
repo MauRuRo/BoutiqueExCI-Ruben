ed28390644ae10c0a9a09edf4f6d476bcc8cb828
MauRuRo <m.rubenderoos@gmail.com>
2020-12-17 12:21:30 +0000

    fix for UserProfile?

--- profiles/models.py
+++ profiles/models.py
@@ -19,7 +19,7 @@ class UserProfile(models.Model):
     default_country = CountryField(blank_label="Country", max_length=40, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.user.username
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
